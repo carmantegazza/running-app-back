af6a30f372c26cfdbb5e7d12c6c6967ef39b389e
JeanetteLorena <k1027.jeanette@gmail.com>
2023-12-04 21:48:38 -0300

    nada

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1190,6 +1190,41 @@ const userControllers = {
     }
   },
 
+
+  
+  updateFavEvent: async (req, res) => {
+
+        try {
+          const userId = req.params.id;
+          const eventId = req.body.eventId;
+      
+          const user = await Users.findOne({ _id: userId });
+      
+          if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+          }
+      
+          if (user.favEvents.includes(eventId)) {
+            return res.status(400).json({ success: false, message: 'The event is already in favourites' });
+          }
+      
+          user.favEvents.push(eventId);
+          const updatedUser = await user.save();
+      
+          return res.status(200).json({ success: true, event: updatedUser });
+        } catch (error) {
+          return res.status(500).json({ success: false, message: 'Internal server error' });
+        }
+      },
+      getUser: async(req, res) => {
+        try {
+            const route = await Users.findById(req.params.id)
+            return res.status(200).json({success:true, message: 'User found', route: route})
+        } catch (error) {
+            return res.status(500).json({success:false})
+        }
+    },
+
 }
 
 module.exports = userControllers
